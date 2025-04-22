59de8d0a96d2567e0d695b821db4119a3c497734
deanroberts <deano.roberts@menloschool.org>
2025-04-22 14:56:19 -0700

    WHen the cookie is clicked, it changes colors

--- src/Cookie.java
+++ src/Cookie.java
@@ -1,4 +1,137 @@
-package PACKAGE_NAME;
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Cookie {
+/**
+ * Ball
+ * Written by Steinberg, Thibodeaux, & Sanden for Intro Programming
+ * Modified for CS2 by Zach Blick on Feb. 13, 2024
+ *
+ * Ball is designed to create a graphical Ball for MouseDemo.
+ */
+
+public class Cookie
+{
+    // Instance Variables
+    private int x;              // Center x
+    private int y;              // Center y
+    private int dx;             // delta x in one time unit
+    private int dy;             // delta y in one time unit
+    private int radius;         // Radius of the ball
+    private Color color;
+
+    // Constructors
+    public Cookie() {
+        this(200, 300,5,4, 100, Color.RED);
+    }
+
+    public Cookie(int xIn, int yIn, int dxIn, int dyIn, int radiusIn, Color colorIn) {
+        x = xIn;
+        y = yIn;
+        dx = dxIn;
+        dy = dyIn;
+        radius = radiusIn;
+        color = colorIn;
+    }
+
+    public boolean isClicked(int x, int y) {
+        double dx = (this.x - x) * (this.x - x);
+        double dy = (this.y - y) * (this.y - y);
+        return Math.sqrt(dx + dy) <= radius;
+    }
+
+    public void move() {
+        x = x + dx;
+        y = y + dy;
+    }
+
+    public boolean bounce(int xLow, int xHigh, int yLow, int yHigh) {
+        // Check for an x bounce.  Note that we bounce if the x is too
+        //  low or too high AND IS HEADING IN THE WRONG DIRECTION.
+        if ((x - radius <= xLow && dx < 0) || (x + radius >= xHigh && dx > 0)) {
+            dx = -dx;
+            return true;
+        }
+
+        // Now check for a y bounce.
+        if ((y - radius <= yLow && dy < 0) || (y + radius >= yHigh && dy > 0)) {
+            dy = -dy;
+            return true;
+        }
+        return false;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setCenter(int xIn, int yIn) {
+        x = xIn;
+        y = yIn;
+    }
+
+    public void setColor(Color colorIn)
+    {
+        color = colorIn;
+    }
+
+    public int getCenterX()
+    {
+        return x;
+
+    }
+
+    public int getCenterY()
+    {
+        return y;
+
+    }
+
+    public int getRadius()
+    {
+        return radius;
+
+    }
+
+    /**
+     * Shift the ball by the given amount in the x direction.
+     * @param shift     How much to shift the ball's location
+     */
+    public void shiftX(int shift, int xLow, int xHigh) {
+        if (x - radius + shift <= xLow && shift < 0) {
+            x = xLow + radius;
+        }
+        else if (x + radius + shift >= xHigh && shift > 0) {
+            x = xHigh - radius;
+        }
+        else {
+            x += shift;
+        }
+    }
+
+    /**
+     * Shift the ball by the given amount in the y direction.
+     * @param shift     How much to shift the ball's location
+     */
+    public void shiftY(int shift, int yLow, int yHigh) {
+        if (y - radius + shift <= yLow && shift < 0) {
+            y = yLow + radius;
+        }
+        else if (y + radius + shift >= yHigh && shift > 0) {
+            y = yHigh - radius;
+        }
+        else {
+            y += shift;
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x - radius, y - radius, 2 * radius, 2 * radius);
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,140 @@
-package PACKAGE_NAME;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class Game {
+import static java.lang.Math.sqrt;
+
+/**
+ * MouseDemo and MouseDemoView
+ * Written by Steinberg, Thibodeaux, & Sanden for Intro Programming
+ * Modified for CS2 by Zach Blick on Feb. 13, 2024
+ *
+ * This demo implements the MouseListener and MouseMotionListener interfaces. The front-end listens for the
+ * mouse events, which in turn call the methods that are implemented in the back-end.
+ */
+
+public class Game implements MouseListener, MouseMotionListener {
+
+    public static final Color[] COLORS = {Color.RED, Color.BLUE, Color.GREEN};
+
+    // Instance Variables
+    private Cookie cookie;
+    private int clickNum;
+    private GameView window;
+
+    // Constructors
+    public Game() {
+        // Create a Ball with the 0 parameter constructor
+        this.cookie = new Cookie();
+        this.clickNum = 0;
+
+        // Initialize the front-end
+        this.window = new GameView(this);
+
+        // addMouseListener attaches a MouseListener object to MouseDemoView
+        // addMouseMotionListener attaches a MouseMotionListener object to MouseDemoView
+        // By passing "this" as the parameter, it makes this specific Instance
+        // of MouseDemo the MouseListener and the MouseMotionListener, which means
+        // the front-end senses the mouse, but the back-end has the methods.
+        this.window.addMouseListener(this);
+        this.window.addMouseMotionListener(this);
+    }
+
+    public Cookie getCookie()
+    {
+        return cookie;
+    }
+    public static void main(String[] args) {
+        Game md = new Game();
+    }
+
+    /********************************************
+     MouseListener event handlers - BEGIN
+     ********************************************/
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // Change the color
+        clickNum++;
+        cookie.setColor(COLORS[clickNum % 3]);
+
+        window.repaint();
+
+        // For demo purposes only
+        System.out.println("mousePressed event handler executed.");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // For demo purposes only
+        System.out.println("mouseReleased event handler executed.");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // For demo purposes only
+        int xVal = e.getX();
+        int yVal = e.getY();
+        int centerX = cookie.getCenterX();
+        int centerY = cookie.getCenterY();
+        double distance = Math.sqrt(sqrt(xVal - centerX) + sqrt(yVal - centerY));
+
+        if (distance <= cookie.getRadius())
+        {
+            cookie.setColor(Color.BLUE);
+        }
+        window.repaint();
+
+
+        System.out.printf("mouseClicked event handler executed for click number: %d\n", clickNum);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // For demo purposes only
+        System.out.println("mouseEntered event handler executed.");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // For demo purposes only
+        System.out.println("mouseExited event handler executed.");
+    }
+
+    /********************************************
+     MouseListener event handlers - END
+     ********************************************/
+
+    /********************************************
+     MouseMotionListener event handlers - BEGIN
+     ********************************************/
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // Have the ball follow the dragging mouse
+        // Print to terminal for demo purposes only
+        System.out.println("\t\t\texecuting mouseDragged event handler");
+
+        // Ask the input event the current location (x and y position on the Frame) of the mouse
+        int x = e.getX();
+        int y = e.getY();
+
+        // If the ball is clicked
+        if (cookie.isClicked(x, y)) {
+            // Move the ball and repaint.
+            cookie.setCenter(x, y);
+            window.repaint();
+        }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // For demo purposes only
+        System.out.println("\t\t\texecuting mouseMoved event handler");
+    }
+
+    /********************************************
+     MouseMotionListener event handlers - END
+     ********************************************/
 }
